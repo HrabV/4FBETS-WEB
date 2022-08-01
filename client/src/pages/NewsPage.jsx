06f424cef7eb65e66afd0b261e5e6b4431b16f5d
Vasyl Hrab <basilgrosse83@gmail.com>
2022-08-01 13:47:02 +0300

    addNewsReducer

--- client/src/pages/NewsPage.jsx
+++ client/src/pages/NewsPage.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import { NewsListItem } from "../components/newspage/NewsListItem";
 
 import { MainLayout } from "../layouts";
-
-export const NewsPage = () => (
-  <MainLayout>
-    <NewsListItem></NewsListItem>
-  </MainLayout>
-);
+import axios from "axios";
+export const NewsPage = () => {
+  useEffect(() => {
+    const test = async () => {
+      const response = await axios.post("https://localhost:7072/api/test");
+      console.log(response.data);
+    };
+    test();
+  }, []);
+  return (
+    <MainLayout>
+      <NewsListItem></NewsListItem>
+    </MainLayout>
+  );
+};
