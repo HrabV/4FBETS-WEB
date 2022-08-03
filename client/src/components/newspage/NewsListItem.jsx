3b9f6150dbe5db2e0a3d0069edf38255472746bb
Vasyl Hrab <basilgrosse83@gmail.com>
2022-08-03 13:05:07 +0300

    news page

--- client/src/components/newspage/NewsListItem.jsx
+++ client/src/components/newspage/NewsListItem.jsx
@@ -1,32 +1,99 @@
 import {
-  Box,
   Center,
   Container,
   Flex,
-  HStack,
-  list,
   SimpleGrid,
-  Stack,
   TableContainer,
   Wrap,
   WrapItem,
+  Grid,
+  GridItem,
+  LinkBox,
+  LinkOverlay,
+  Heading,
+  Text,
+  Box,
+  Image,
+  HStack,
+  VStack,
+  Stack,
 } from "@chakra-ui/react";
+
 import { NewsItem } from "./NewsItem";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchNews, selectAllNews } from "../../reducers/newsSlice";
+const imagePath = "https://localhost:7072/images/";
+
+const NewsOption = ({ news }) => {
+  return (
+    <LinkBox
+      bgColor={"white"}
+      boxShadow={"base"}
+      _hover={{ boxShadow: "dark-lg" }}
+      ml={5}
+      as="article"
+      p="5"
+      borderWidth="1px"
+      rounded="md"
+      width={"150px"}
+      height={"250px"}
+    >
+      <Grid gridTemplateRows={"50% 20% 1fr"}>
+        <GridItem>
+          <Box key={news.id}>
+            <Image
+              alt={"ItemNews Image"}
+              fit={"cover"}
+              align={"center"}
+              src={imagePath + news.imageUrl}
+            ></Image>
+          </Box>
+        </GridItem>
+        <GridItem>
+          <Box p="2">
+            <Text fontSize={"10px"}>2022-03-07</Text>
+          </Box>
+        </GridItem>
+        <GridItem>
+          <Box>
+            <Heading fontSize={"10px"}>{news.title}</Heading>
+          </Box>
+        </GridItem>
+      </Grid>
+      <LinkOverlay href="/news/:newsId"></LinkOverlay>
+    </LinkBox>
+  );
+};
 
 export const NewsListItem = () => {
-  const test = new Array(<NewsItem />, <NewsItem />, <NewsItem />);
-  const elements = test.map((item) => {
-    return item;
-  });
+  const dispatch = useDispatch();
+  const news = useSelector(selectAllNews);
+
+  const newsStatus = useSelector((state) => state.news.status);
+
+  useEffect(() => {
+    if (newsStatus === "idle") {
+      dispatch(fetchNews());
+    }
+  }, [newsStatus, dispatch]);
+
+  let content;
+
+  if (newsStatus === "succeeded") {
+    const orderedNews = news
+      .slice()
+      .sort((a, b) => b.title.localeCompare(a.title));
+
+    content = orderedNews.map((news) => (
+      <NewsOption key={news.id} news={news} />
+    ));
+  }
+
   return (
-    <>
-      <Box>
-        <Stack direction={{ base: "column", lg: "row" }}>
-          <NewsItem />
-          <NewsItem />
-          <NewsItem />
-        </Stack>
-      </Box>
-    </>
+    <Stack direction={{ base: "column", lg: "row" }} p="2">
+      {content}
+    </Stack>
   );
 };
